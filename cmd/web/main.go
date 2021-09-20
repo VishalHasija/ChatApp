671af090b6f09e36e90ed813355d57b7e15d76c4
Vishal Hasija <vishal.hasijagetweave.com@Vishals-MacBook-Pro.local>
2021-09-20 08:01:36 +0530

    Running WS Channel to listen to any request

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/VishalHasija/ChatApp/internal/handlers"
 )
 
 func main() {
 	routes := routes()
 
-	log.Println("Starting web server on port 8080")
+	log.Println("starting Channel listener")
+	go handlers.ListenToWsChannel()
 
+	log.Println("Starting web server on port 8080")
 	_ = http.ListenAndServe(":8080", routes)
 }
